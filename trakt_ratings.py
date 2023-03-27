3de9d64a20ab873139e8049956cc90668a29fd5e
zeroquinc <39315068+zeroquinc@users.noreply.github.com>
2023-03-27 21:55:59 +0200

    add Show rating support

--- trakt_ratings.py
+++ trakt_ratings.py
@@ -30,6 +30,7 @@ timezone = pytz.timezone(config['timezone'])
 episode_spoiler = config['episode_spoiler']
 season_spoiler = config['season_spoiler']
 movie_spoiler = config['movie_spoiler']
+show_spoiler = config['show_spoiler']
 
 # TMDB API endpoint URLs
 search_url = "https://api.themoviedb.org/3/search/multi"
@@ -38,6 +39,96 @@ search_url = "https://api.themoviedb.org/3/search/multi"
 episode_ratings_url = f"https://api.trakt.tv/users/{trakt_username}/ratings/episodes"
 movie_ratings_url = f"https://api.trakt.tv/users/{trakt_username}/ratings/movies"
 season_ratings_url = f"https://api.trakt.tv/users/{trakt_username}/ratings/seasons"
+show_ratings_url = f"https://api.trakt.tv/users/{trakt_username}/ratings/shows"
+
+def send_show_notification(title, description, trakt_url, show_title, tmdb_id, show_slug, imdb_id, trakt_id):
+    
+    # Search for show or movie by title
+    search_params = {
+        "api_key": api_key,
+        "query": show_title
+    }
+    search_response = requests.get(search_url, params=search_params)
+
+    # Parse JSON response for search results
+    search_data = json.loads(search_response.text)
+
+    # Check if search results exist
+    if len(search_data["results"]) > 0:
+        if search_data["results"][0]["poster_path"] is not None:
+            poster_url = "https://image.tmdb.org/t/p/w500" + search_data["results"][0]["poster_path"]
+        else:
+            poster_url = None
+    else:
+        poster_url = None
+
+    # Add headers for Trakt API authorization
+    headers = {
+        "Content-Type": "application/json",
+        "trakt-api-version": "2",
+        "trakt-api-key": client_id
+    }
+    
+    # Fetch show overview
+    show_params = {
+        "extended": "full"
+    }
+    show_response = requests.get(f"https://api.trakt.tv/shows/{trakt_id}", headers=headers, params=show_params)
+    show_data = json.loads(show_response.text)
+    
+    # Get the overview for the show data
+    if show_data.get('overview'):
+        overview = show_data['overview']
+        spoiler = "||" if show_spoiler else ""  # Set spoiler based on show_spoiler value
+    else:
+        overview = "No overview available"
+        spoiler = ""
+        
+    # Create embedded notification
+    author_name = "Trakt: Show Rated"
+
+    embed = {
+        "title": title,
+        "color": 13506070,
+        "timestamp": datetime.utcnow().isoformat(),
+        "author": {
+            "name": author_name,
+            "icon_url": "https://i.imgur.com/poGtHrf.png"
+        },
+        "fields": [
+            {
+                "name": "Rating",
+                "value": f"{description} :star:",
+                "inline": True
+            },
+            {
+                "name": "User",
+                "value": f"[{trakt_username}](https://trakt.tv/users/{trakt_username})",
+                "inline": True
+            },
+            {
+                "name": "Details",
+                "value": f"[Trakt]({trakt_url}) / [IMDb](https://imdb.com/title/{imdb_id})",
+                "inline": True
+            },
+            {
+                "name": "Overview",
+                "value": f"{spoiler}{overview}{spoiler}",
+                "inline": True
+            }
+        ]
+    }
+
+    # Add poster to embedded notification
+    if poster_url:
+        embed["thumbnail"] = {"url": poster_url}
+    # Create Discord notification payload
+    payload = {
+        "embeds": [embed]
+    }
+
+    # Send notification to Discord webhook
+    requests.post(webhook_url, json=payload).headers
 
 def send_episode_notification(title, description, episode_slug, season, episode_number, trakt_url, show_title, tmdb_id, imdb_id):
     
@@ -345,6 +436,29 @@ def get_recent_ratings():
         "end_at": end_time
     }
     movie_response = requests.get(movie_ratings_url, headers=headers, params=movie_params)
+
+     #Fetch recent show ratings
+    show_params = {
+        "start_at": start_time,
+        "end_at": end_time
+    }
+    show_response = requests.get(show_ratings_url, headers=headers, params=show_params)
+    
+    # Parse JSON response for show ratings
+    show_data = json.loads(show_response.text)
+    for show in show_data:
+        rating_time = parser.parse(show["rated_at"]).astimezone(timezone)
+        if current_time - rating_time <= timedelta(minutes=1):
+            show_title = show["show"]["title"]
+            show_slug = show["show"]["ids"]["slug"]
+            trakt_id = show["show"]["ids"]["trakt"]
+            imdb_id = show["show"]["ids"]["imdb"]
+            show_year = show["show"]["year"]
+            title = f"{show_title} ({show_year})"
+            description = show["rating"]
+            trakt_url = f"https://trakt.tv/shows/{show_slug}"
+            tmdb_id = show["show"]["ids"]["tmdb"]
+            send_show_notification(title, description, trakt_url, show_title, tmdb_id, show_slug, imdb_id, trakt_id)
 
     # Parse JSON response for episode ratings
     episode_data = json.loads(episode_response.text)
